b605b02f914a752d59778c149b0e7343513b978e
swiftwolff <jeff.hsu@west.cmu.edu>
2014-03-19 22:08:09 -0700

    new solution

--- ChOne8.java
+++ ChOne8.java
@@ -1,50 +1,46 @@
-import java.util.Arrays;
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChOne8 {
 	
-	public static boolean isSubstring(String one, String two){
-		
-		if(one.length()>=two.length()){
-			return false;
-		}
-		int onelen = one.length();
-		int count = 0;
-		int tmpOneIndex = 0;
-		char[] tmpOne = one.toCharArray();
-		char[] tmpTwo = two.toCharArray();
+	public static boolean isSubstring(String pat, String txt) {
+		  Map<Character, Integer> mt = new HashMap<Character, Integer>();
+		  // Bad character Skip : Moore Table Construction
+		  for (int i = 0; i < pat.length(); i++) {
+		   mt.put(pat.charAt(i), i);
+		  }
+		  int skip = 0;
+		  int N = txt.length();
+		  int M = pat.length();
+		  for (int i = 0; i <= N-M; i= i+ skip) {
+		   skip = 0;
+		   for (int j = M - 1; j >= 0; j--) {
+		    if (pat.charAt(j) != txt.charAt(i+j)) {
+		     if(mt.get(txt.charAt(i + j))!=null)
+		      skip = Math.max(1, j - mt.get(txt.charAt(i + j)));
+		     else
+		      skip = j+1;
+		     break;
+		    }
+		   }
+		   if (skip == 0) {
+		    // Match found
+		    return true;
+		   }
+		  }
+		  return false;
+		 }
 
-		for(int i=0;i<two.length();i++){
-			if(count>0&&tmpOne[tmpOneIndex]==tmpTwo[i]){
-				count++;
-				continue;
-			}else{
-				count=0;
-				tmpOneIndex = 0;
-			}
-			if(tmpOne[tmpOneIndex]==tmpTwo[i]){
-				count++;
-				tmpOneIndex++;
-			}
-			if(tmpOneIndex==tmpOne.length){
-				return true;
-			}
-		}
-		
-		if (count!=onelen){
-			return false;
-		}
-		return true;
-	}
-	
 	public static boolean checkRotation(String one, String two){
-		return true;
+		one = one+one;
+		System.out.println(one);
+		System.out.println(two);
+		return isSubstring(two,one);
 	}
 	
 	public static void main(String args[]){
-		String a = "hello";
-		String b = "eo";
-		
-		System.out.println(isSubstring(b,a));
+		String a = "heyhey";
+		String b = "jim_saw_me_in_a_barber_parlour";
+		System.out.println(isSubstring(a,b));
 	}
 }
