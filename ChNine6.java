7c900efb44e69404b8e122459dbf16379e465751
swiftwolff <jeff.hsu@west.cmu.edu>
2014-04-04 23:37:09 -0700

    new solutions

--- ChNine6.java
+++ ChNine6.java
@@ -37,6 +37,33 @@ public class ChNine6 {
 		return;
 	}
 	
+	public void addParen(ArrayList<String> list, int leftRem, int rightRem, char[] str,int count){
+		if(leftRem < 0||rightRem <leftRem){
+			return; //invalid state
+		}
+		if(leftRem==0&&rightRem==0){//no more paren left
+			String s = String.copyValueOf(str);
+			list.add(s);
+		}else{
+			if(leftRem > 0){
+				str[count] = '(';
+				addParen(list,leftRem -1,rightRem,str,count+1);
+			}
+			
+			if(rightRem>leftRem){
+				str[count]=')';
+				addParen(list,leftRem,rightRem-1,str,count+1);
+			}
+		}
+	}
+	
+	public ArrayList<String> generateParens(int count){
+		char[] str = new char[count*2];
+		ArrayList<String> list = new ArrayList<String>();
+		addParen(list,count,count,str,0);
+		return list;
+	}
+	
 	public static void main(String args[]){
 		System.out.println(generate(3));
 	}
--- ChNine9.java
+++ ChNine9.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 public class ChNine9 {
 	
-	public static char[][] nQueens(int number){
+	public static ArrayList<char[][]> nQueens(int number){
 		char[][] board = new char[number][number];
 		initalize(board);
 		ArrayList<char[][]> res = new ArrayList<char[][]>();
 		for(int i=0;i<1;i++){
 			for(int j=0;j<board[0].length;j++){
 				board[i][j] = 'Q';
-				nQueensHelper(board,0,res);
+				System.out.println("Hey");
+				nQueensHelper(board,j,1,res);
 				board[i][j] = '.';
 			}
 		}
 		
-		return board;
+		return res;
 	}
 	
 	public static void initalize(char[][] board){
@@ -24,12 +25,33 @@ public class ChNine9 {
 		}
 	}
 	
-	public static void nQueensHelper(char[][] board,int row, ArrayList<char[][]> res){
+	public static void nQueensHelper(char[][] board,int precolumn,int row, ArrayList<char[][]> res){
 		if(row==board.length){
+			System.out.println("Here");
 			res.add(board);
 		}
+		int check = 0;
 		//needs to clean the board
-		
+		for(int i=0;i<row;i++){
+			for(int j=0;j<board[0].length;j++){
+				System.out.println("yo");
+				if(board[i][j]!='Q'){
+					continue;
+				}
+				if(precolumn!=j&&Math.abs(row-j)!=Math.abs(i-precolumn)){
+					board[row][j] = 'Q';
+					nQueensHelper(board,precolumn,row+1,res);
+				}else{
+					check++;
+					break;
+				}
+				if(check==1){
+					check = 0;
+					break;
+				}
+			}
+			
+		}
 		
 		
 		return;
@@ -37,6 +59,6 @@ public class ChNine9 {
 	
 	
 	public static void main(String args[]){
-		System.out.println('Q');
+		System.out.println(nQueens(3));
 	}
 }
